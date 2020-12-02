4f106e2bd8cbeb705f300739bdbf905417ab6318
Anna <rosalenanna@gmail.com>
2020-12-02 18:57:53 +0100

    milestone 1

--- js/myscript.js
+++ js/myscript.js
@@ -9,23 +9,23 @@ var app = new Vue({
     apikey:'536b4c4fda91efaf8dcd925d42f6d67d'
   },
 
-  mounted: function(){
-    axios.get(api,
-      { params:
-        {
-          api_key: this.apikey,
-          language: "it-IT" ,
-          query: "casa" ,
-          page: 1 ,
-          include_adult: false
-        }
-      }
-    )
-    .then(response => {
-      this.arrayMovies = response.data.results;
-      console.log(response.data.results);
-    })
-  },
+  // mounted: function(){
+  //   axios.get(api,
+  //     { params:
+  //       {
+  //         api_key: this.apikey,
+  //         language: "it-IT" ,
+  //         query: "casa" ,
+  //         page: 1 ,
+  //         include_adult: false
+  //       }
+  //     }
+  //   )
+  //   .then(response => {
+  //     this.arrayMovies = response.data.results;
+  //     console.log(response.data.results);
+  //   })
+  // },
 
   methods:{
     cerca: function(){
@@ -42,7 +42,15 @@ var app = new Vue({
       )
       .then(response => {
         this.arrayMovies = response.data.results;
-        console.log(this.arrayMovies);
+        // for (var i = 0; i < this.arrayMovies.length; i++) {
+        //   console.log(this.arrayMovies[i].title)
+        //   console.log(this.arrayMovies[i].original_title);
+        // }
+        // this.arrayMovies.forEach((element,i)=>{
+        //   console.log(this.arrayMovies[i].title);
+        //   console.log(this.arrayMovies[i].original_title);
+        // })
+
       })
     }
   }
