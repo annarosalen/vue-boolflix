21bca120c2d43dfb591d7349cd61a7b199843bcc
Anna <rosalenanna@gmail.com>
2020-12-02 17:35:06 +0100

    push

---
+++ js/myscript.js
@@ -0,0 +1,49 @@
+const api = "https://api.themoviedb.org/3/search/movie";
+
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    search:"",
+    arrayMovies: [],
+    apikey:'536b4c4fda91efaf8dcd925d42f6d67d'
+  },
+
+  mounted: function(){
+    axios.get(api,
+      { params:
+        {
+          api_key: this.apikey,
+          language: "it-IT" ,
+          query: "casa" ,
+          page: 1 ,
+          include_adult: false
+        }
+      }
+    )
+    .then(response => {
+      this.arrayMovies = response.data.results;
+      console.log(response.data.results);
+    })
+  },
+
+  methods:{
+    cerca: function(){
+      axios.get(api,
+        { params:
+          {
+            api_key: this.apikey,
+            language: "it-IT" ,
+            query: this.search ,
+            page: 1 ,
+            include_adult: false
+          }
+        }
+      )
+      .then(response => {
+        this.arrayMovies = response.data.results;
+        console.log(this.arrayMovies);
+      })
+    }
+  }
+});
