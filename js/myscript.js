00acc3ef25372f2b325dc8e3f210e403d43a6fdc
Anna <rosalenanna@gmail.com>
2020-12-04 16:01:33 +0100

    bg image problema

--- js/myscript.js
+++ js/myscript.js
@@ -4,8 +4,8 @@ const api = "https://api.themoviedb.org/3/search/multi";
 var app = new Vue({
   el: "#app",
   data: {
+    poster:"",
     yellowstar: "bgyellow",
-    newVote:"",
     search:"",
     arrayAll: [],
     apikey:'536b4c4fda91efaf8dcd925d42f6d67d',
@@ -53,6 +53,7 @@ var app = new Vue({
           // trasformo i voti DA (1 a 10)  A  (1 a 5)
           this.arrayAll[i].vote_average = Math.floor(this.arrayAll[i].vote_average / 2);
 
+
           // ***** BANDIERE ****
           // condizione per output bandiera inglese
           if (this.arrayAll[i].original_language === 'en') {
@@ -61,11 +62,18 @@ var app = new Vue({
           // trasformo tutte le lingue in bandiere
           this.arrayAll[i].original_language = 'https://www.countryflags.io/' + this.arrayAll[i].original_language + '/flat/24.png';
 
+
+
           // ***** POSTER ****
-          // creo link intero immagini poster
-          this.arrayAll[i].poster_path = 'https://image.tmdb.org/t/p/' + 'w185' + this.arrayAll[i].poster_path;
-          
+          // creo link intero immagini poster Milestone 3
+          // this.arrayAll[i].poster_path = 'https://image.tmdb.org/t/p/' + 'w185' + this.arrayAll[i].poster_path;
+
+          // Milestone 4 poster copertina
+          this.poster = 'https://image.tmdb.org/t/p/' + 'w185' + this.arrayAll[i].poster_path;
+          console.log(this.poster);
+
         }); //fine ciclo forEach
+
 
       })
     },
