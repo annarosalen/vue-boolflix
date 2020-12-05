b4de7368bf25985c17850339721e3b0036551906
Anna <rosalenanna@gmail.com>
2020-12-05 17:58:39 +0100

    Milestone 4

--- js/myscript.js
+++ js/myscript.js
@@ -10,23 +10,23 @@ var app = new Vue({
     apikey:'536b4c4fda91efaf8dcd925d42f6d67d',
   },
 
-  // mounted: function(){
-  //   axios.get(api,
-  //     { params:
-  //       {
-  //         api_key: this.apikey,
-  //         language: "it-IT" ,
-  //         query: "casa" ,
-  //         page: 1 ,
-  //         include_adult: false
-  //       }
-  //     }
-  //   )
-  //   .then(response => {
-  //     this.arrayMovies = response.data.results;
-  //     console.log(response.data.results);
-  //   })
-  // },
+  mounted: function(){
+    axios.get(api,
+      { params:
+        {
+          api_key: this.apikey,
+          language: "it-IT" ,
+          query: "casa" ,
+          page: 1 ,
+          include_adult: false
+        }
+      }
+    )
+    .then(response => {
+      this.arrayAll = response.data.results;
+      this.loop();
+    })
+  },
 
   methods:{
     cerca: function(){
@@ -42,39 +42,45 @@ var app = new Vue({
         }
       )
       .then(response => {
-        // copio array per i film como API
+        // copio array per i film come API
         this.arrayAll = response.data.results;
 
+        // quando cerco film l'input torna vuoto
+        this.search = "";
 
-        this.arrayAll.forEach((element,i)=>{
-
-          // ***** VOTI ****
-          // trasformo i voti DA (1 a 10)  A  (1 a 5)
-          this.arrayAll[i].vote_average = Math.floor(this.arrayAll[i].vote_average / 2);
-
-
-          // ***** BANDIERE ****
-          // condizione per output bandiera inglese
-          if (this.arrayAll[i].original_language === 'en') {
-            this.arrayAll[i].original_language = 'gb';
-          }
-          // trasformo tutte le lingue in bandiere
-          this.arrayAll[i].original_language = 'https://www.countryflags.io/' + this.arrayAll[i].original_language + '/flat/24.png';
-
-
-
-          // ***** POSTER ****
-          // creo link intero immagini poster
-          this.arrayAll[i].poster_path = 'https://image.tmdb.org/t/p/' + 'w342' + this.arrayAll[i].poster_path;
-          console.log(this.poster);
-
-        }); //fine ciclo forEach
-
+        this.loop();
 
       })
     }, //fine funzione cerca
 
+    loop: function(){
+      this.arrayAll.forEach((element,i)=>{
 
+        // ***** VOTI ****
+        // trasformo i voti DA (1 a 10)  A  (1 a 5)
+        this.arrayAll[i].vote_average = Math.floor(this.arrayAll[i].vote_average / 2);
+
+
+        // ***** BANDIERE ****
+        // condizione per output bandiera inglese
+        if (this.arrayAll[i].original_language === 'en') {
+          this.arrayAll[i].original_language = 'gb';
+        }
+        // trasformo tutte le lingue in bandiere
+        this.arrayAll[i].original_language = 'https://www.countryflags.io/' + this.arrayAll[i].original_language + '/flat/24.png';
+
+
+        // ***** POSTER ****
+        // creo link intero immagini poster
+        this.arrayAll[i].poster_path = 'https://image.tmdb.org/t/p/' + 'w185' + this.arrayAll[i].poster_path;
+
+        // se manca l'img del poster la sostituisco
+        if(this.arrayAll[i].poster_path === 'https://image.tmdb.org/t/p/' + 'w185' + 'null'){
+          this.arrayAll[i].poster_path = 'https://img.freepik.com/vettori-gratuito/film-strip-sfondo_23-2147516042.jpg?size=338&ext=jpg';
+        }
+
+      }); //fine ciclo forEach
+    }
 
   }
 });
